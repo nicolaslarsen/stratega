e39e2523bb2c3333547752897046960ce60cfcd7
Nicolas <nicolasringsmose@hotmail.com>
2020-01-27 22:47:49 +0100

    serverside push so I have better examples

--- TapOne/static/StratBook/js/strat_creation.js
+++ TapOne/static/StratBook/js/strat_creation.js
@@ -6,6 +6,13 @@ $(document).ready(function(){
             /bullet_set-[0-9]/g, bullet_set_id_text));
     $('#id_bullet_set-TOTAL_FORMS').val(parseInt(form_index) + 1);
     $('#delete_bullet_set-' + form_index).on('click', removeClick);
+
+    $('#id_' + bullet_set_id_text + '-text').on('focus', function(){
+      var tmp = $(this).val();
+      $(this).val('');
+      $(this).val(tmp);
+    });
+    $('#id_' + bullet_set_id_text + '-text').focus();
   });
 
   var removeClick = function(){

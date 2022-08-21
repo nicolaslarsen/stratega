22d89d562948f9e0384f9c18e426fac638f37255
Nicolas <nicolasringsmose@hotmail.com>
2022-08-21 20:32:40 +0200

    another rename

--- Strategiga/Strategiga/asgi.py
+++ Strategiga/Strategiga/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Stratega project.
+ASGI config for Strategiga project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stratega.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Strategiga.settings')
 
 application = get_asgi_application()
--- Strategiga/Strategiga/wsgi.py
+++ Strategiga/Strategiga/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Stratega project.
+WSGI config for Strategiga project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Stratega.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Strategiga.settings')
 
 application = get_wsgi_application()

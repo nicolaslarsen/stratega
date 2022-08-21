bf31d34676c5f56ed56c8862ad03015742f6e6eb
Nicolas <nicolasringsmose@hotmail.com>
2022-08-21 19:10:35 +0200

    cleanup + gitignore

--- TapOne/AdminPage/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Admin(models.Model):
-    class Meta:
-        permissions = (
-            ('can_swap_strats', 'Can swap the strategies between two players'),
-            ('can_see_hidden_strats', 'Can see hidden strategies'),
-        )
--- TapOne/StratBook/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StratbookConfig(AppConfig):
-    name = 'StratBook'
